22f5c637e1cc9f24f5cd39f1c8271c103c42b918
Eric Tung <eric.p.tung@gmail.com>
2017-11-16 16:53:18 -0800

    Completed fibs

--- Javascript/src/algorithms.js
+++ Javascript/src/algorithms.js
@@ -65,7 +65,11 @@ Algorithms.sumRec = function (numbers) {
 
 // Write a function which returns the first n elements from the fibonnacci sequence, given n.
 Algorithms.fibs = function (number) {
-
+ if(number === 1) { return [0] }
+ if(number === 2) { return [0, 1] }
+ let prev = Algorithms.fibs(number - 1)
+ prev.push(prev[prev.length-1]+ prev[prev.length-2])
+ return prev;
 };
 
 // Write a function that takes a string and returns true if it's a palindrome, false if it's not.
